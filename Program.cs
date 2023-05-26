4af6a69163524adab8c8bcdd591093c7404e011f
Garyvo <olanantonio100@gmail.com>
2023-05-26 08:22:03 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Prueva.Services;
+using Prueva.Entites;
+
+namespace Prueva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario user = new Usuario();
+            Operacion op1 = new Operacion();
+
+            double total = op1.compra();
+            Console.WriteLine("El monto final de tu compra es de: " + total);
+            
+            user.mostrar();
+        }
+    }
+}
